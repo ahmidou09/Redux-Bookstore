5b81a745d503ad7d95facea5e04455bcace2e7ff
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-08-22 20:28:42 +0100

    Add Redux Toolkit setup and state slices

--- src/components/Home/BookList.js
+++ src/components/Home/BookList.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import IndividualBook from './IndividualBook';
 
-function BookList({ books, onDelete }) {
+function BookList() {
+  const { books } = useSelector((state) => state.book);
+
   return (
     <div className="book-list">
       {books.map((book) => (
@@ -10,21 +12,11 @@ function BookList({ books, onDelete }) {
           key={book.id}
           title={book.title}
           author={book.author}
-          onDelete={() => onDelete(book.id)}
+          id={book.id}
         />
       ))}
     </div>
   );
 }
-
-BookList.propTypes = {
-  books: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
-  onDelete: PropTypes.func.isRequired,
-};
 
 export default BookList;
